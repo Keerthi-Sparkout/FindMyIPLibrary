3d8894c22f8e7ad1bf3b8b3b126e29273cdfe985
keerthiv <keerthi@sparkouttech.com>
2024-08-31 12:59:59 +0530

    Initial Commit

---
+++ Sources/FindMyIPLibrary/IPDetailsContentView.swift
@@ -0,0 +1,91 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by apple on 31/08/24.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+public struct ContentView: View {
+    @StateObject var viewModel: IpDetailsViewModel = .init(network: NetworkService())
+    @State var isLoading: Bool = true
+    @State var errorMessage: String = ""
+    
+    public var body: some View {
+        VStack {
+            Text("IP Details")
+                .font(.title)
+                .fontWeight(.semibold)
+            if isLoading {
+                ProgressView()
+            } else {
+                if errorMessage.isEmpty  {
+                    VStack(alignment: .leading, spacing: 8) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("IP Address:")
+                                .font(.headline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            Text(viewModel.details?.ip ?? "")
+                                .fontWeight(.regular)
+                                .foregroundColor(.gray)
+                        }.background(Color.white)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Version:")
+                                .font(.headline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            Text(viewModel.details?.version ?? "")
+                                .fontWeight(.regular)
+                                .foregroundColor(.gray)
+                        }.background(Color.white)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("City:")
+                                .font(.headline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            Text(viewModel.details?.city ?? "")
+                                .fontWeight(.regular)
+                                .foregroundColor(.gray)
+                        }.background(Color.white)
+                        
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Country:")
+                                .font(.headline)
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            Text(viewModel.details?.country ?? "")
+                                .fontWeight(.regular)
+                                .foregroundColor(.gray)
+                        }.background(Color.white)
+                    }
+                   
+                } else {
+                    Text(errorMessage)
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .onAppear() {
+            viewModel.fetchIDetailsAPI { result in
+                isLoading = false
+                switch result {
+                case .success(let model):
+                    viewModel.details = model
+                case .failure(let error):
+                    errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+}
+
+
+//#Preview {
+//    SwiftUIView()
+//}
---
+++ Sources/FindMyIPLibrary/IpDetailsViewModel.swift
@@ -0,0 +1,63 @@
+//
+//  IpDetailsViewModel.swift
+//  FindMyIP
+//
+//  Created by apple on 31/08/24.
+//
+
+import Foundation
+import Alamofire
+import Combine
+
+@available(iOS 13.0, *)
+public  protocol NetworkDataProtocol {
+    func fetchIpDetails(url: String) -> AnyPublisher<IPDetailsModel, AFError>
+}
+
+@available(iOS 13.0, *)
+public struct NetworkService: NetworkDataProtocol {
+    public func fetchIpDetails(url: String) -> AnyPublisher<IPDetailsModel, AFError> {
+        guard let url = URL(string: url) else {
+            fatalError("Bad url")
+        }
+        return AF.request(url, method: .get)
+            .validate()
+            .publishDecodable(type: IPDetailsModel.self, decoder: JSONDecoder())
+            .value()
+            .eraseToAnyPublisher()
+    }
+}
+
+
+@available(iOS 13.0, *)
+public class IpDetailsViewModel: ObservableObject {
+   
+    @Published public var details: IPDetailsModel?
+    var network: NetworkDataProtocol
+    var cancellable = Set<AnyCancellable>()
+    
+    public init(network: NetworkDataProtocol) {
+        self.network = network
+    }
+    
+    public func fetchIDetailsAPI(completion: @escaping (Result<IPDetailsModel, AFError>) -> Void) {
+        network.fetchIpDetails(url: "https://ipapi.co/json/")
+            .receive(on: DispatchQueue.main)
+            .sink { result in
+                print(result)
+                switch result {
+                case .failure(let error):
+                    completion(.failure(error))
+                case .finished:
+                    break
+//                    completion(.success(error))
+                    
+                }
+            } receiveValue: { model in
+                completion(.success(model))
+            }.store(in: &cancellable)
+        
+        
+    }
+    
+}
